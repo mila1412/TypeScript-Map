27a233d933da69f2bea1875d9813e04f721ca8c4
milaaa <k120240305@gmail.com>
2025-01-27 16:57:34 +0800

    新增隨機用戶與公司地圖標記

---
+++ src/CustomMap.ts
@@ -0,0 +1,37 @@
+import { User } from "./User";
+import { Company } from "./Company";
+
+export class CustomMap {
+  private googleMap: google.maps.Map;
+
+  constructor(divId: string) {
+    this.googleMap = new google.maps.Map(
+      document.getElementById(divId) as HTMLElement,
+      {
+        zoom: 1,
+        center: { lat: 0, lng: 0 },
+        mapId: "DEMO_MAP_ID",
+      }
+    );
+  }
+
+  addUserMaker(user: User): void {
+    new google.maps.marker.AdvancedMarkerElement({
+      map: this.googleMap,
+      position: {
+        lat: user.location.lat,
+        lng: user.location.lng,
+      },
+    });
+  }
+
+  addCompanyMaker(company: Company): void {
+    new google.maps.marker.AdvancedMarkerElement({
+      map: this.googleMap,
+      position: {
+        lat: company.location.lat,
+        lng: company.location.lng,
+      },
+    });
+  }
+}
